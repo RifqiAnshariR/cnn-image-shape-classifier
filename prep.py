5eb8a1b2001662722cb5a3777a13a52c0e0284cb
RifqiAnshariR <ansharirifqi2@gmail.com>
2025-03-24 05:33:07 +0700

    Feat: Add split dataset feature

--- prep.py
+++ prep.py
@@ -1,19 +1,27 @@
 import torch
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
-def get_data_loader(root="Dataset", batch_size=32):
+def get_data_loaders(root, batch_size, split_ratio):
     transform = transforms.Compose([
         transforms.Resize((64, 64)),
         transforms.ToTensor()
     ])
     
     dataset = ImageFolder(root=root, transform=transform)
-    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    
-    return data_loader, dataset.classes
+
+    # Split dataset train (80%) dan test (20%)
+    train_size = int(split_ratio * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader, dataset.classes
 
 if __name__ == "__main__":
-    loader, classes = get_data_loader()
+    train_loader, test_loader, classes = get_data_loaders("Dataset", 32, 0.8)
     print("Dataset loaded with classes:", classes)
+    print(f"Train samples: {len(train_loader.dataset)}, Test samples: {len(test_loader.dataset)}")
