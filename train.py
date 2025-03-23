2b069f7c1aeaea1b3c350f3c3a6addbeb27f6d6e
RifqiAnshariR <ansharirifqi2@gmail.com>
2025-03-24 05:38:52 +0700

    Refactor: Improve code structure; Feat: Add 1 conv. layer and train loss

--- train.py
+++ train.py
@@ -2,38 +2,45 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from prep import get_data_loader
+from prep import get_data_loaders
 
 class ShapeClassifier(nn.Module):
     def __init__(self, num_classes=3):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 8, 3, padding=1)
+        # 2 Conv. layers.
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc = nn.Linear(8 * 32 * 32, num_classes)
+        self.fc = nn.Linear(32 * 16 * 16, num_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
 
-def train_model(epochs=50, lr=0.001):
-    train_loader, classes = get_data_loader()
+def train_model(epochs, lr, model_path):
+    train_loader, _, classes = get_data_loaders()
     model = ShapeClassifier(num_classes=len(classes))
+    
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
     for epoch in range(epochs):
+        total_loss = 0
         for images, labels in train_loader:
             optimizer.zero_grad()
             outputs = model(images)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-        print(f"Epoch {epoch+1}/{epochs} selesai")
+            total_loss += loss.item()
 
-    torch.save(model.state_dict(), "./model/shape_classifier.pth")
-    print("Model saved!")
+        print(f"Epoch {epoch+1}/{epochs}, Loss: {total_loss / len(train_loader):.4f}")
+
+    torch.save(model.state_dict(), model_path)
+    print(f"Model saved to {model_path}")
 
 if __name__ == "__main__":
-    train_model()
+    train_model(50, 0.001, "./Model/shape_classifier.pth")
